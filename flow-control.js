3b4b892a87de1adb2078d0a57876a14bd1f5f30e
Geno M <giantpowpow2@gmail.com>
2019-07-25 09:06:17 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -16,7 +16,7 @@ function ageChecker(age) {
   if (age >= 13 && age <= 19) {
     return "You are a teenager!";
   } else if (age < 13) {
-    return "You are not a teenager";
+    return "You are a kid";
   } else {
     return "You are a grownup";
   }
@@ -29,29 +29,27 @@ function ternaryTeenager(age) {
 function switchAge(age) {
   switch(age) {
     case 13:
-      console.log("You are a teenager");
+      return "You are a teenager";
       break;
     case 14:
-      console.log("You are a teenager");
+      return "You are a teenager";
       break;
     case 15:
-      console.log("You are a teenager");
+      return "You are a teenager";
       break;
     case 16:
-      console.log("You are a teenager");
+      return "You are a teenager";
       break;
     case 17:
-      console.log("You are a teenager");
+      return "You are a teenager";
       break;
     case 18:
-      console.log("You are a teenager");
+      return "You are a teenager";
       break;
     case 19:
-      console.log("You are a teenager");
+      return "You are a teenager";
       break;
     default:
-      console.log("You have an age");
+      return "You have an age";
   }
 }
-
-basicTeenager(14);
